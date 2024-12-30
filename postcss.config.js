87641d3a1b14daf80300f0d0848102513522ee14
Rens Salaladaga <staleexam19@gmail.com>
2024-12-30 21:42:52 +0800

    chore: configure base code

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+
+
+
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        cssnano: {},
+        autoprefixer: {}
+    }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { NoPageComponent } from './pages/no-page/no-page.component';
+import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LandingPageComponent
+    },
+    {
+        path: "about",
+        component: AboutPageComponent
+    },
+
+
+    {
+        path: "login",
+        component: SignInPageComponent
+    },
+
+    {
+        path: "registration",
+        component: SignUpPageComponent
+    },
+
+
+    // Insert diri ang mga role specific routes
+       // 
+
+
+    // 404 page
+    {
+        path: "**",
+        component: NoPageComponent
+    }
+];
