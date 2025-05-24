5c47a56ba357dda8cff8fd84b62a2f8cd284b802
Rens Salaladaga <staleexam19@gmail.com>
2025-05-25 06:29:50 +0800

    fix: admin profile setting side nav

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -4,15 +4,14 @@ import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { UserManagementComponent } from "./pages/user-management/user-management.component";
 import { CurriculumManagementComponent } from "./pages/curriculum-management/curriculum-management.component";
 import { SubjectDetailPageComponent } from "./pages/subject-detail-page/subject-detail-page.component";
-import { DocEditorComponent } from "./pages/doc-editor/doc-editor.component";
 import { QuizCreationFormComponent } from "./pages/quiz-creation-form/quiz-creation-form.component";
-import { ProfileSettingsComponent } from "../student/pages/profile-settings/profile-settings.component";
 import { LessonContentComponent } from "./pages/lesson-content/lesson-content.component";
 import { EditLessonComponent } from "./pages/edit-lesson/edit-lesson.component";
 import { SubjectLessonsComponent } from "./pages/subject-lessons/subject-lessons.component";
 import { TeacherSubjectComponent } from "./pages/teacher-subject/teacher-subject.component";
 import { EnrolledStudentsComponent } from "./pages/enrolled-students/enrolled-students.component";
 import { UserProfileComponent } from "./pages/user-profile/user-profile.component";
+import { ProfileSettingsComponent } from "./pages/profile-settings/profile-settings.component";
 
 const routes: Routes = [
   {
